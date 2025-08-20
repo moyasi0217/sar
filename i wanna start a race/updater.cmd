491f80d8563c0527d3a3bf4fb0a9029da6abbb0f
moyasi <105961405+moyasi0217@users.noreply.github.com>
2025-08-21 01:06:36 +0900

    Add files via upload

---
+++ i wanna start a race/updater.cmd
@@ -0,0 +1,7 @@
+@echo off
+curl -O https://raw.githubusercontent.com/moyasi0217/sar/refs/heads/main/i%%20wanna%%20start%%20a%%20race/version.txt
+set /p tag=<"version.txt"
+curl -O https://raw.githubusercontent.com/moyasi0217/sar/refs/heads/main/i%%20wanna%%20start%%20a%%20race/IWannaStartARace.exe
+curl -O https://raw.githubusercontent.com/moyasi0217/sar/refs/heads/main/i%%20wanna%%20start%%20a%%20race/data.win
+curl -O https://raw.githubusercontent.com/moyasi0217/sar/refs/heads/main/i%%20wanna%%20start%%20a%%20race/readme.txt
+curl -O https://raw.githubusercontent.com/moyasi0217/sar/refs/heads/main/i%%20wanna%%20start%%20a%%20race/options.ini
